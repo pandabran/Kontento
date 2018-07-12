5ca2b325a8249ccca9977cfb402f5746e4e9013a
juanpursigido <june.genelsa@gmail.com>
2018-07-12 07:47:39 +0700

    updated calendar UI

--- app/src/main/java/com/example/dell/kontento/activities/Calendar.java
+++ app/src/main/java/com/example/dell/kontento/activities/Calendar.java
@@ -1,27 +1,41 @@
 package com.example.dell.kontento.activities;
 
 import android.annotation.SuppressLint;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.example.dell.kontento.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class Calendar extends AppCompatActivity {
 
     private static final String TAG = "CalendarActivity";
     private CalendarView mCalendarView;
+    String date_n;
+    SimpleDateFormat time_n;
+    TextView date,time, today_is_text;
 
-    @SuppressLint("ResourceType")
+    @SuppressLint({"ResourceType", "SimpleDateFormat"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         mCalendarView = (CalendarView) findViewById(R.layout.activity_calendar);
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+        date_n =  new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+        time_n = new SimpleDateFormat("hh:mm a");
 
+        date = (TextView) findViewById(R.id.cdate);
+        time = (TextView)findViewById(R.id.ctime);
 
-
+        date.setText(date_n);
+        time.setText(time_n.format(calendar.getTime()));
+        today_is_text = (TextView)findViewById(R.id.today_is_text);
     }
 }
